292e01ee0036aa03cfba5d6f22571b110aa4d777
github username <github email address>
2021-08-11 13:40:45 -0400

    commit tree 58e3a292ef2a84ef6ee83b9178de583f1d9a7192

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+      };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+      //console.log(object)
+      let objId= object["id"]
+      document.body.append(objId)
+
+  })
+  .catch(function(error) {
+    alert("Something went wrong!");
+    document.body.append(error.message);
+  });
+
+}
